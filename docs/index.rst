1915648cc8401f16373ed7383074d479c652f7c9
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-08-28 20:42:57 +0200

    Initial port of the Multi-PIE database as a satellite package for bob

---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+.. vim: set fileencoding=utf-8 :
+.. Laurent El Shafey <laurent.el-shafey@idiap.ch>
+.. Mon 13 Aug 2012 12:36:40 CEST 
+
+====================
+ Multi-PIE Database
+====================
+
+.. automodule:: xbob.db.multipie
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <laurent.el-shafey@idiap.ch>
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='xbob.db.multipie',
+    version='1.0.0a1',
+    description='Multi-PIE Database Access API for Bob',
+    url='http://github.com/bioidiap/xbob.db.multipie',
+    license='GPLv3',
+    author='Laurent El Shafey',
+    author_email='laurent.el-shafey@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+      'setuptools',
+      'bob',  # base signal proc./machine learning library
+    ],
+
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+      ],
+
+    entry_points = {
+      # bob database declaration
+      'bob.db': [
+        'multipie = xbob.db.multipie.driver:Interface',
+        ],
+
+      # bob unittest declaration
+      'bob.test': [
+        'multipie = xbob.db.multipie.test:MultipieDatabaseTest',
+        ],
+      },
+
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
+)
