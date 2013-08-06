976422ac85b2eb0a71c9484cf997edcd496b58cb
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-06 19:09:11 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.multipie',
-    version='1.0.7',
+    version='1.0.8a0',
     description='Multi-PIE Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.multipie',
     license='GPLv3',
